479d97e80e386ad352fad90b45feed95c8c70e6e
Willie <m4ds31@gmail.com>
2020-01-04 20:42:47 +0330

    errors 101-104 added | occuranceLine added to tables

--- ANTLR_COOL_Program/ANTLR_COOL_Program/SymbolTable/Table.java
+++ ANTLR_COOL_Program/ANTLR_COOL_Program/SymbolTable/Table.java
@@ -12,14 +12,18 @@ public class Table {
     public String type;
     public String id;
     public String[] properties;
+    public int occuranceLine = 0;
+    public static Table mainMethod;
 
-    public Table(ParserRuleContext ctx, Table par, String type, String id, String[] props){
+    public Table(ParserRuleContext ctx, Table par, String type, String id, String[] props, int line){
         this.ctx = ctx;
         parent = par;
         decs = new LinkedList<Table>();
         this.type = type;
         this.id = id;
         properties = props;
+        occuranceLine = line;
+        mainMethod = null;
     }
 
     public void print(int intent){
@@ -29,6 +33,79 @@ public class Table {
         else System.out.println(properties[0]);
         for(Table t : decs){
             t.print(intent + 1);
+        }
+    }
+
+    public void CheckDecs(){
+        CheckClassDoubleDec();
+        CheckFeatureDoubleDec();
+        MainFuncExistanceCheck();
+        if(mainMethod == null){
+            System.out.println("Error104 : Can not find main method");
+        }
+    }
+
+    protected void MainFuncExistanceCheck(){
+        for(Table t : decs){
+            if(t.type.equals("Method") && t.id.equals("main")){
+                mainMethod = t;
+            }
+            else{
+                t.MainFuncExistanceCheck();
+            }
+        }
+    }
+
+    private void CheckClassDoubleDec(){
+        for(Table t : decs){
+            if(t.type == "Class"){
+                CheckClassDuplicate(t);
+            }
+        }
+    }
+
+    private void CheckClassDuplicate(Table cls){
+        boolean foundItself = false;
+        for(Table table : decs){
+            if(table == cls) {
+                foundItself = true;
+                continue;
+            }
+
+            if(!foundItself && table.type == "Class"){
+                if(table.id.equals(cls.id)){
+                    System.out.println("Error101 in line "+ cls.occuranceLine + " , " + cls.id + " has been defined already");
+                }
+            }
+        }
+    }
+
+    protected void CheckFeatureDoubleDec(){
+        for(Table t : decs){
+            if(t.type == "Method" || t.type == "Property"){
+                CheckFeatureDuplicate(t, false);
+            }
+            else{
+                t.CheckFeatureDoubleDec();
+            }
+        }
+    }
+
+    protected void CheckFeatureDuplicate(Table feat, boolean foundItself){
+        for(Table table : decs){
+            if(!foundItself && table == feat){
+                foundItself = true;
+                continue;
+            }
+
+            if(!foundItself && table.id.equals(feat.id)){
+                String tmp = table.type == "Method"?"102":"103";
+                System.out.println("Error" + tmp + ": in line " + feat.occuranceLine + " , " + feat.id + " has been defined already");
+            }else{
+                for(Table dec : table.decs){
+                    dec.CheckFeatureDuplicate(feat, foundItself);
+                }
+            }
         }
     }
 }
